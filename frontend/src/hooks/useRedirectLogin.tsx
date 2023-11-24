6ff8c3fbd1478b9918bd367c901a17b79c802ed5
KotaTakeishi <takeishi.kota.373@s.kyushu-u.ac.jp>
2023-11-24 21:04:58 +0900

    add: 不完全だけど，リダイレクト機能追加

---
+++ frontend/src/hooks/useRedirectLogin.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../pages/firebase';
+
+/**
+ * Redirects to /signin if the user is not logged in.
+ * TODO: でも一瞬セキュアなページが見えちゃうから改善したい
+ */
+export function useRequireLogin() {
+    const router = useRouter();
+
+    useEffect(() => {
+        return onAuthStateChanged(auth, (user) => {
+            if (!user) {
+                router.push('/signin');
+            }
+        });
+    }, [router]);
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,19 @@
-import { useRouter } from 'next/router'
-import { signOut } from "firebase/auth";
-import { auth } from "./firebase"
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "./firebase";
+import { useRequireLogin } from "../hooks/useRedirectLogin";
 
 export default function Home() {
+  useRequireLogin();
   const router = useRouter();
-  const user = auth.currentUser;
+  const [user, setUser] = useState(auth.currentUser);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, []);
 
   const handleClick = () => {
     signOut(auth).then(() => {
@@ -19,5 +28,5 @@ export default function Home() {
       <h1>{user?.email}</h1>
       <button onClick={handleClick}>Sign Out</button>
     </div>
-  )
+  );
 }
