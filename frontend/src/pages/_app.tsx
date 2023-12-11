58a58d39e44cf2b39e52eecd9ceeecbbd2beaee1
taiseee <taisei7548@gmail.com>
2023-12-11 16:41:53 +0900

    update:navbarの追加

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,12 +4,14 @@ import type { AppProps } from 'next/app'
 import { AuthProvider } from '@/provider/AuthProvider';
 import { AuthGurad } from '@/guard/AuthGuard';
 import { ChakraProvider } from '@chakra-ui/react';
+import Navbar from '@/components/Nabvar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <AuthProvider>
         <AuthGurad>
+          <Navbar/>
           <Component {...pageProps} />
         </AuthGurad>
       </AuthProvider>
