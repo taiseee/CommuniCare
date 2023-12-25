3eb93386b9d713c8ded78a4510fe5673231c3d60
taiseee <taisei7548@gmail.com>
2023-12-26 04:04:17 +0900

    add: firestoreの導入

--- frontend/src/lib/firebaseConfig.ts
+++ frontend/src/lib/firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,3 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore();
+if (process.env.NODE_ENV === 'development') {
+    connectFirestoreEmulator(db, 'localhost', 8080);
+}
