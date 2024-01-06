0712194fb0673cb4cc23870ca6680d31cf7fe6f9
taiseee <taisei7548@gmail.com>
2024-01-06 18:48:35 +0900

    update: グループ作成時にイベントをレコメンドするように変更

--- functions/src/service.py
+++ functions/src/service.py
@@ -3,6 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 from langchain.output_parsers import PydanticOutputParser
 from .schema import EventList
+from firebase_admin import firestore
+import random
 
 class EventExtractor:
     def __init__(self, model="gpt-3.5-turbo-1106"):
@@ -30,3 +32,49 @@ def get_webtxt(url):
     soup = BeautifulSoup(response.text, 'html.parser')
     webtxt = soup.get_text()
     return webtxt
+
+def regular_execution_recommend_events():
+    db = firestore.client()
+
+    events_ref = db.collection('events')
+    events = events_ref.order_by('createdAt', direction=firestore.Query.DESCENDING).limit(20).get()
+    event_ids = []
+    for event in events:
+        event_ids.append(event.id)
+
+    groups = db.collection('groups').get()
+
+    group_events_ref = db.collection('groupEvents')
+
+    for group in groups:
+        group_id = group.id
+        # イベントidをランダムに1件取得
+        event_id = random.choice(event_ids)
+        # グループイベントを作成
+        group_events_ref.add({
+            'groupId': group_id,
+            'eventId': event_id,
+            'createdAt': firestore.SERVER_TIMESTAMP,
+            'updatedAt': firestore.SERVER_TIMESTAMP
+        })
+
+def first_recommend_events(group_id: str):
+    db = firestore.client()
+
+    events_ref = db.collection('events')
+    events = events_ref.order_by('createdAt', direction=firestore.Query.DESCENDING).limit(20).get()
+    event_ids = []
+    for event in events:
+        event_ids.append(event.id)
+
+    group_events_ref = db.collection('groupEvents')
+
+    # イベントidをランダムに1件取得
+    event_id = random.choice(event_ids)
+    # グループイベントを作成
+    group_events_ref.add({
+        'groupId': group_id,
+        'eventId': event_id,
+        'createdAt': firestore.SERVER_TIMESTAMP,
+        'updatedAt': firestore.SERVER_TIMESTAMP
+    })
