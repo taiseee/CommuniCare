c77d9b4d75e58ec0273bc3935f916d9113b80fe5
taiseee <taisei7548@gmail.com>
2024-01-11 20:03:59 +0900

    add: イベントレコメンドの定期実行

---
+++ functions/src/api/recommend_event_on_schedule.py
@@ -0,0 +1,42 @@
+from firebase_functions import scheduler_fn
+from firebase_admin import firestore
+from firebase_admin import firestore
+import random
+
+@scheduler_fn.on_schedule(
+    schedule="every friday 16:00",
+    timezone=scheduler_fn.Timezone("Asia/Tokyo"),
+    memory=512,
+)
+def recommend_event_on_schedule(schedule_event: scheduler_fn.ScheduledEvent) -> None:
+    db = firestore.client()
+
+    events_ref = db.collection('events')
+    events = events_ref.order_by('createdAt', direction=firestore.Query.DESCENDING).limit(20).get()
+    event_ids = []
+    for event in events:
+        event_ids.append(event.id)
+
+    if len(event_ids) == 0:
+        return None
+
+    groups = db.collection('groups').get()
+
+    group_events_ref = db.collection('groupEvents')
+
+    if len(groups) == 0:
+        return None
+
+    for group in groups:
+        group_id = group.id
+        # イベントidをランダムに1件取得
+        event_id = random.choice(event_ids)
+        # グループイベントを作成
+        group_events_ref.add({
+            'groupId': group_id,
+            'eventId': event_id,
+            'createdAt': firestore.SERVER_TIMESTAMP,
+            'updatedAt': firestore.SERVER_TIMESTAMP
+        })
+
+    return None
--- functions/src/service.py
+++ functions/src/service.py
@@ -34,37 +34,6 @@ class EventExtractor:
         webtxt = soup.get_text()
         return webtxt
 
-def regular_execution_recommend_events():
-    db = firestore.client()
-
-    events_ref = db.collection('events')
-    events = events_ref.order_by('createdAt', direction=firestore.Query.DESCENDING).limit(20).get()
-    event_ids = []
-    for event in events:
-        event_ids.append(event.id)
-
-    if len(event_ids) == 0:
-        return
-
-    groups = db.collection('groups').get()
-
-    group_events_ref = db.collection('groupEvents')
-
-    if len(groups) == 0:
-        return
-
-    for group in groups:
-        group_id = group.id
-        # イベントidをランダムに1件取得
-        event_id = random.choice(event_ids)
-        # グループイベントを作成
-        group_events_ref.add({
-            'groupId': group_id,
-            'eventId': event_id,
-            'createdAt': firestore.SERVER_TIMESTAMP,
-            'updatedAt': firestore.SERVER_TIMESTAMP
-        })
-
 def first_recommend_events(group_id: str):
     db = firestore.client()
 
