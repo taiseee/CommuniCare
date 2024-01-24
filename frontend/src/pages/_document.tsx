4296b55ea42e6b979fd4a8dd751022eb6bf2b0e7
KotaTakeishi <takeishi.kota.373@s.kyushu-u.ac.jp>
2024-01-24 15:30:12 +0900

    fix: google翻訳無効化

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,10 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
-        <Html lang="en">
+        <Html lang="ja">
             <Head>
                 <link rel="manifest" href="/manifest.json" />
                 <meta name="theme-color" content="#000" />
+                <meta http-equiv="Content-Language" content="ja" />
+                <meta name="google" content="notranslate" />
             </Head>
             <body>
                 <Main />
